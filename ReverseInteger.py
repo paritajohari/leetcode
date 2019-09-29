1130361dd2ea1749b55a70cf2c8806307b67f45d
paritajohari <johariparita@gmail.com>
2019-09-29 14:02:39 +0530

    Prob2 ReverseInteger

---
+++ ReverseInteger.py
@@ -0,0 +1,37 @@
+'''Given a 32-bit signed integer, reverse digits of an integer.
+
+Example 1:
+
+Input: 123
+Output: 321
+
+Example 2:
+
+Input: -123
+Output: -321
+
+Example 3:
+
+Input: 120
+Output: 21
+
+Note:
+Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [-231,  231 - 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.'''
+
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x < 0:
+            x_str=str(abs(x))
+            y= int(x_str[::-1])*-1
+        else:
+            x_str=str(x)
+            y= int(x_str[::-1])
+        if(y>=(-2**31) and y<=(2**31)-1):
+            return y
+        else:
+            return 0
+        
