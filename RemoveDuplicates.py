39b28f75faba03e779f9481fb699ca6b38f20ba5
paritajohari <paritajohari@users.noreply.github.com>
2020-06-23 00:14:17 +0530

    Two pointer approach

--- RemoveDuplicates.py
+++ RemoveDuplicates.py
@@ -1,19 +1,30 @@
-class Solution(object):
-    def removeDuplicates(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        idx = 0
-        while(idx < len(nums)):
-            jdx = idx + 1            
-            while(jdx < len(nums)):
-                if nums[idx] == nums[jdx]:
-                    nums.remove(nums[jdx])                    
-                else:
-                    break
-            idx = idx + 1
+# class Solution(object):
+#     def removeDuplicates(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: int
+#         """
+#         idx = 0
+#         while(idx < len(nums)):
+#             jdx = idx + 1            
+#             while(jdx < len(nums)):
+#                 if nums[idx] == nums[jdx]:
+#                     nums.remove(nums[jdx])                    
+#                 else:
+#                     break
+#             idx = idx + 1
             
+           
+# Solution 2:
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        start = 0
+        N = len(nums)
+        for i in range(1, N):
+            if nums[start] != nums[i]:
+                nums[start + 1] = nums[i]
+                start += 1
+        return len(nums[:start+1])
                     
         
         
